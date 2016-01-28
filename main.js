c3d2bef679901865571607cade1e8d08b0f7f328
mwarnsley <marcus.warnsley@gmail.com>
2016-01-28 00:04:18 -0500

    structuring out the object in javascript using JSON Format

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-ECHO is on.
+var obj = {
+    "person":[{
+        "fname":"marcus",
+        "lname":"warnsley",
+        "age":"28",
+        "dob":"02161987",
+        "email":"marcus.warnsley@gmail.com",
+        "movieGenre":"horror",
+        "favFood":"chinese"
+    }]
+};
